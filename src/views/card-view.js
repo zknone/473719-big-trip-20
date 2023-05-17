34e8856111250d22c29805cc3ac18795ad40ce2d
Streltsov Ivan <principdereva@gmail.com>
2023-05-17 21:14:55 +0200

    добавил переключение фейворит

--- src/views/card-view.js
+++ src/views/card-view.js
@@ -19,6 +19,8 @@ class CardView extends View {
   handleClick(event) {
     if (event.target.closest('.event__rollup-btn')) {
       this.notify('open');
+    } else if (event.target.closest('.event__favorite-btn')) {
+      this.notify('favorite');
     }
   }
 
